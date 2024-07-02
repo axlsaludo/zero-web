1b06a07d3fea7878df1914686558f35fb43d1832
Kurt Axl Saludo <79072016+axlsaludo@users.noreply.github.com>
2024-07-02 10:03:53 +0800

    Update

--- db/sessionCheck.php
+++ db/sessionCheck.php
@@ -4,7 +4,7 @@ session_start(); // Start the session
 // Check if the user is not logged in
 if (!isset($_SESSION['user_id'])) {
     // Redirect to the login page or another appropriate page
-    header("Location: ../components/login.html"); // Adjust the path as needed
+    header("Location: ../components/404.html"); // Adjust the path as needed
     exit; // Ensure no further code execution after redirection
 }
 ?>
