22a1fff92f75d6e799d8b805f0119c6685dc2d35
Kurt Axl Saludo <79072016+axlsaludo@users.noreply.github.com>
2024-07-03 09:52:08 +0800

    New Directories

--- API/login.php
+++ API/login.php
@@ -29,7 +29,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                     $_SESSION['is_admin'] = true;
                     header("Location: ../pages/dashboard.html");
                 } else {
-                    header("Location: ../pages/landing.html");
+                    header("Location: ../pages/data.html");
                 }
                 exit;
             } else {
