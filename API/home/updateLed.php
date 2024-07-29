98907fc29167abde4846a9e22bbed6c6107d37c0
Kurt Axl Saludo <79072016+axlsaludo@users.noreply.github.com>
2024-07-29 21:15:26 +0800

    Update updateLed.php

--- API/home/updateLed.php
+++ API/home/updateLed.php
@@ -1,4 +1,11 @@
 <?php
+
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json");
+header("Access-Control-Allow-Methods: GET");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
 include '../../db/Database.php';
 
 $db = new Database();
